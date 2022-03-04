ef577169495d599b274b3bfd42189e1a0f861443
Athos Schrapett <athosschrapett@outlook.com>
2022-03-03 22:27:26 -0300

    Acerto metodos

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -1,6 +1,64 @@
-﻿namespace RestWithASPNETUdemy.Services.Implementations
+﻿using RestWithASPNETUdemy.Models;
+
+namespace RestWithASPNETUdemy.Services.Implementations
 {
-    public class PersonServiceImplementation
+    public class PersonServiceImplementation : IPersorService
     {
+        private int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(Person person)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new();
+            for(int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = 1,
+                FirstName = "Leandro",
+                LastName = "Costa",
+                Address = "Uberlandia - Minas Gerais - Brasil",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet() + i,
+                FirstName = "Person Name" + i,
+                LastName = "Person Last Name" + i,
+                Address = "Some Address" + i,
+                Gender = "Male" + i
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
     }
 }
