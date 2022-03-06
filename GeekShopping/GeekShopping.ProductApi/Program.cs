fce519c98c2798adcca2c1340e3954099f50e264
Athos Schrapett <athosschrapett@outlook.com>
2022-03-06 18:00:52 -0300

    Inclusão Migrations

--- GeekShopping/GeekShopping.ProductApi/Program.cs
+++ GeekShopping/GeekShopping.ProductApi/Program.cs
@@ -1,3 +1,7 @@
+using GeekShopping.ProductApi.Model;
+using GeekShopping.ProductApi.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +11,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connection = builder.Configuration["PostgresConnection:PostgresConnectionString"];
+builder.Services.AddNpgsql<PostgresContext>(connection);
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
