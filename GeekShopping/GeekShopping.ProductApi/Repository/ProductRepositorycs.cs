3259cdd80ba23698fa68a4f96c280eee5d85dcb0
Athos Schrapett <athosschrapett@outlook.com>
2022-03-12 23:28:44 -0300

    injeção de dependencias

--- GeekShopping/GeekShopping.ProductApi/Repository/ProductRepositorycs.cs
+++ GeekShopping/GeekShopping.ProductApi/Repository/ProductRepositorycs.cs
@@ -1,6 +1,43 @@
-﻿namespace GeekShopping.ProductApi.Repository
+﻿using AutoMapper;
+using GeekShopping.ProductApi.Data.ValueObjects;
+using GeekShopping.ProductApi.Model.Context;
+
+namespace GeekShopping.ProductApi.Repository
 {
-    public class ProductRepositorycs
+    public class ProductRepositorycs : IProductRepository
     {
+        private readonly PostgresContext _context;
+        private IMapper _mapper;
+
+        public ProductRepositorycs(PostgresContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public Task<ProductVO> Create(ProductVO vo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductVO>> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductVO> FindById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductVO> Update(ProductVO vo)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
