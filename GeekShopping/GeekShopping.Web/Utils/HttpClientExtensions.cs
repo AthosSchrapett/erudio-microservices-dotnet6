d3d7e6d95e3078941a446f093882d63e6616c78e
Athos Schrapett <athosschrapett@outlook.com>
2022-03-16 23:06:57 -0300

    Inclusão metodos PostAsJson e PutAsJson

--- GeekShopping/GeekShopping.Web/Utils/HttpClientExtensions.cs
+++ GeekShopping/GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -16,5 +16,20 @@ namespace GeekShopping.Web.Utils
                 PropertyNameCaseInsensitive = true,
             });
         }
+
+        public static Task<HttpResponseMessage> PostAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return httpClient.PostAsync(url, content);
+        }
+        public static Task<HttpResponseMessage> PutAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return httpClient.PutAsync(url, content);
+        }
     }
 }
