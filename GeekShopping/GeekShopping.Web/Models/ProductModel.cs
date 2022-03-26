6863f160bb4a676149d8e9c148859e52d9ce6119
Athos Schrapett <athosschrapett@outlook.com>
2022-03-26 09:16:02 -0300

    Correção Pagina de produtos

--- GeekShopping/GeekShopping.Web/Models/ProductModel.cs
+++ GeekShopping/GeekShopping.Web/Models/ProductModel.cs
@@ -3,10 +3,10 @@
     public class ProductModel
     {
         public long Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public decimal Price { get; set; }
-        public string Description { get; set; }
-        public string CategoryName { get; set; }
-        public string ImagemUrl { get; set; }
+        public string? Description { get; set; }
+        public string? CategoryName { get; set; }
+        public string? ImagemUrl { get; set; }
     }
 }
