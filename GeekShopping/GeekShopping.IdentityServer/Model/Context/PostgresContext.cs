d0389a483699dbec684228ef5038d96c7f212a5e
Athos Schrapett <athosschrapett@outlook.com>
2022-04-03 10:08:55 -0300

    Inclusão DbContext

---
+++ GeekShopping/GeekShopping.IdentityServer/Model/Context/PostgresContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.IdentityServer.Model.Context
+{
+    public class PostgresContext : IdentityDbContext<ApplicationUser>
+    {
+		public PostgresContext(DbContextOptions<PostgresContext> options) : base(options) { }
+	}
+}
